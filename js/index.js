81be162315294b7d5ad5b99e8beceab28c200fae
sami-aloosi <kingxsam@gmail.com>
2020-07-07 13:56:45 -0700

    event 7 done

--- js/index.js
+++ js/index.js
@@ -1,52 +1,61 @@
 /** @format */
 
+//click functions
+const letterL = (e) => {
+  (e.target.innerText = "L"), (e.target.style.backgroundColor = "#0AD8D5");
+};
+
+const letterO = (e) => {
+  (e.target.innerText = "O"), (e.target.style.backgroundColor = "#0AD8D5");
+};
+
 // 1 click
-
 const signUpButtons = document.querySelectorAll("div.btn");
+
 const signUpButton1 = signUpButtons[0];
-signUpButton1.addEventListener("click", () => {
-  (signUpButton1.innerText = "L"),
-    (signUpButton1.style.backgroundColor = "#0AD8D5");
-});
+signUpButton1.addEventListener("click", letterL);
+
 const signUpButton2 = signUpButtons[1];
-signUpButton2.addEventListener("click", () => {
-  (signUpButton2.innerText = "O"),
-    (signUpButton2.style.backgroundColor = "#0AD8D5");
-});
+signUpButton2.addEventListener("click", letterO);
+
 const signUpButton3 = signUpButtons[2];
-signUpButton3.addEventListener("click", () => {
-  (signUpButton3.innerText = "L"),
-    (signUpButton3.style.backgroundColor = "#0AD8D5");
-});
+signUpButton3.addEventListener("click", letterL);
 
+// dblclick functions
+const tomatoBG = (something) => (something.style.backgroundColor = "tomato"); // will give a tomato background-color
+
+const letterB = (e) => {
+  (e.target.innerText = "b"), tomatoBG(e.target);
+};
+
+const letterU = (e) => {
+  (e.target.innerText = "U"), tomatoBG(e.target);
+};
+
+const letterS = (e) => {
+  (e.target.innerText = "S"), tomatoBG(e.target);
+};
 // 2 dblclick
-signUpButton1.addEventListener("dblclick", () => {
-  (signUpButton1.innerText = "B"),
-    (signUpButton1.style.backgroundColor = "tomato");
-});
+signUpButton1.addEventListener("dblclick", letterB);
 
-signUpButton2.addEventListener("dblclick", () => {
-  (signUpButton2.innerText = "U"),
-    (signUpButton2.style.backgroundColor = "tomato");
-});
+signUpButton2.addEventListener("dblclick", letterU);
 
-signUpButton3.addEventListener("dblclick", () => {
-  (signUpButton3.innerText = "S"),
-    (signUpButton3.style.backgroundColor = "tomato");
-});
+signUpButton3.addEventListener("dblclick", letterS);
 
+// mousemove fuctions
+const bigRed = (e) => {
+  (e.target.style.color = "red"), (e.target.style.fontSize = "5rem");
+};
+
+const blueFont = (e) => (e.target.style.color = "skyBlue");
 // 3 mousemove
 
 const logoHeading = document.querySelector("h1");
-logoHeading.addEventListener("mousemove", () => {
-  (logoHeading.style.color = "red"), (logoHeading.style.fontSize = "5rem");
-});
+logoHeading.addEventListener("mousemove", bigRed);
 
 // 4 mouseleave
 
-logoHeading.addEventListener("mouseleave", () => {
-  (logoHeading.style.color = "skyBlue"), (logoHeading.style.fontSize = "5rem");
-});
+logoHeading.addEventListener("mouseleave", blueFont);
 
 // 5 mouseover
 
@@ -57,4 +66,30 @@ allPTags.forEach((data) =>
   })
 );
 
-// 6
+// 6 resize
+const yellowBus = document.querySelector("header img");
+yellowBus.style.margin = "1rem";
+
+window.addEventListener(
+  "resize",
+  () => (yellowBus.style.border = "4px yellow solid")
+);
+
+// 7 load
+
+window.addEventListener(
+  "load",
+  () => (yellowBus.style.border = "4px red solid")
+);
+
+// 8 keydown
+
+document.addEventListener('keydown',function (e) {
+  if (e.key === "+") {
+    allPTags.forEach((data) => data.style.fontSize = "5rem")
+  } else if (e.key === "-"){
+    allPTags.forEach((data) => data.style.fontSize = "1rem")
+  }
+} )
+
+//
